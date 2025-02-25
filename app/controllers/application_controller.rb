4644e99237aa0e2843fceeeb9daa402e44adabc9
naraveni <harshaemployment@gmail.com>
2025-02-25 18:12:15 -0500

    Updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::API
   include JsonWebToken
-  before_action :autheticate_request
-
+  before_action :authenticate_request
   def authenticate_request
     header = request.headers['Authorization']
-    header = request.split(' ').last if header
+    header = header.split(' ').last if header
     decoded = jwt_decode(header)
     @current_user = User.find(decoded[:user_id])
+  rescue JWT::ExpiredSignature, JWT::DecodeError
+    render json: { message: 'Token expired or invalid. Please log in again.' }, status: :unauthorized
   end
 end
---
+++ test/models/visa_experience_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class VisaExperienceTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
